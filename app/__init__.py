991f9a09a34283f8576c3b161d72c40b7a88d763
Malxi <1421215708@qq.com>
2016-09-19 16:46:05 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,7 @@
-try:
-    from flask import Flask
-    app = Flask(__name__)
-    import views
-    # init_menu()
-    # from app.plugins.menu import init_menu
-except:
-    pass
+from flask import Flask
+app = Flask(__name__)
+import views
+# init_menu()
+# from app.plugins.menu import init_menu
 
 
