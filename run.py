1ba472fe584cad111ff95287dfb5ad121ea9d34b
Malxi <13987610292@sina.cn>
2016-09-20 23:05:08 +0800

    modify

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-#!flask/bin/python
+from werkzeug.contrib.fixers import ProxyFix
 
 from app import app
 from app import views
-app.run(debug = True, host = '0.0.0.0', port = 8000)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+app.run(debug = True, host = '0.0.0.0', port = 80)
